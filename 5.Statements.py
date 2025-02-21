aa023fc87b7a75964aa672cfcba884ad9a57876e
Nick <nicholas.papadimitris@gmail.com>
2025-02-22 07:55:49 +1100

    Update 5.Statements.py

--- 5.Statements.py
+++ 5.Statements.py
@@ -11,42 +11,12 @@ import shutil
 today = datetime.now()
 today = today.strftime("%Y-%m-%d")
 
-parent_folder = 'C:/Users/nicho/PycharmProjects/Projects/API2SQL Pipelines/1.2 SimplyAPI_SQL_Pipeline/Statements'  # Replace with your folder path
 
-# Loop through all items in the parent folder
-for item in os.listdir(parent_folder):
-    item_path = os.path.join(parent_folder, item)
-
-    # Check if the item is a directory (folder) and remove it
-    if os.path.isdir(item_path):
-        shutil.rmtree(item_path)  # Deletes the folder and all its contents
-        print(f"Deleted folder: {item_path}")
-
-csv_file_clear = f"C:/Users/nicho/PycharmProjects/Projects/API2SQL Pipelines/1.2 SimplyAPI_SQL_Pipeline/Joined Data/merged_output_Statements_{today}.csv"
-# Check if the file exists, then delete it
-if os.path.exists(csv_file_clear):
-    os.remove(csv_file_clear)
-
-print("All folders deleted successfully.")
 
 # Database connection
-conn = psycopg2.connect(
-    dbname="Simply_API",
-    user="postgres",
-    password="Arxidolemios39",
-    host="localhost",
-    port="5432"
-)
-cursor = conn.cursor()
+
 
 # Simply API setup
-url = "https://api.simplywall.st/graphql"
-headers = {
-    "Authorization": "Bearer sws:Y2VkM2QxYTEtOTA1Mi00ODY2LWIyY2MtNTgyMGFjOWZjMGQ3OmEyYWI1NGU5MDY3MzMyOTE=",
-    "Content-Type": "application/json"
-}
-
-
 
 # Read the CSV file
 df1 = pd.read_csv(f'C:/Users/nicho/PycharmProjects/Projects/API2SQL Pipelines/1.2 SimplyAPI_SQL_Pipeline/Exchanges & Counts/Exchanges_Companies {today}.csv')  # Replace with your actual file path
