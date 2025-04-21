dd7b5872f0a41716aa611100f279b1511ce1bc54
Nick <nicholas.papadimitris@gmail.com>
2025-04-21 23:24:15 +1000

    Update 1.1Get_Exchanges_Counts.py

--- 1.Extract/1.1Get_Exchanges_Counts.py
+++ 1.Extract/1.1Get_Exchanges_Counts.py
@@ -4,6 +4,8 @@ import requests
 from env_utils import *
 from data_flattening_utils import *
 from dir_utils import *
+import pandas as pd
+from sqlalchemy import create_engine
 
 # Fetch the data
 response = requests.post(url, headers=headers, json={"query": query_exchanges})
@@ -15,3 +17,11 @@ statements = data.get('data', {}).get('exchanges', [])
 # Main function to flatten and save the CSV
 flatten_and_save_csv_exchanges(statements, EXCHANGES_CSV_PATH, TODAY)
 
+# Load your CSV
+df = pd.read_csv(EXCHANGES_CSV_PATH)
+
+# Connect to PostgreSQL
+engine = create_engine("....")
+
+# Load into SQL
+df.to_sql("exchanges_counts", engine, if_exists="append", index=False)
