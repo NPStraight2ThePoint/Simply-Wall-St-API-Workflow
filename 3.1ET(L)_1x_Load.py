21f3ed0f2a32a31398d0ad18ad378b445169363b
Nick <nicholas.papadimitris@gmail.com>
2025-04-22 16:35:51 +1000

    Create 3.1ET(L)_1x_Load.py

---
+++ 3.1ET(L)_1x_Load.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from dir_utils import *
+from load_utils import *
+from env_utils import *
+from datetime import datetime
+from post_qa_utils import *
+
+unique_directories = {
+    "insider_transactions": "C:/.../Data/Joined Data/NEW_DATA",
+    "exchanges_counts": "C:/.../Data/Exchanges_Counts"
+}
+
+# Process files for each base directory (excluding Unique directories)
+for base_directory in BASE_DIRECTORIES:
+    process_csv_files(base_directory)
+
+# Process Unique files separately
+for special_type, special_directory in unique_directories.items():
+    process_csv_files(special_directory, only_special=special_type)
+
+
+engine = create_engine(f"postgresql+psycopg2://{db_params['username']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['database']}")
+update_multiple_tables_to_uppercase(db_params)
+delete_data_from_table(db_params)
